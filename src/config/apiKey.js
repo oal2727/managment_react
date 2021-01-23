9d66b2f411b875ca1447130865a781aadf346277
Dash166 <41652885+oal2727@users.noreply.github.com>
2021-01-23 14:54:42 -0500

    Update apiKey.js

--- src/config/apiKey.js
+++ src/config/apiKey.js
@@ -4,17 +4,18 @@ import firebase from 'firebase'
 import 'firebase/firestore'
 import 'firebase/storage'
 var firebaseConfig = {
-    apiKey: "AIzaSyCiPEiAFF3e9pImoYPW9qjTqGtrg7qpZR4",
-    authDomain: "managment-3e95d.firebaseapp.com",
-    databaseURL: "https://managment-3e95d.firebaseio.com",
-    projectId: "managment-3e95d",
-    storageBucket: "managment-3e95d.appspot.com",
-    messagingSenderId: "1079495218764",
-    appId: "1:1079495218764:web:2fa50134e5dddb175486e9",
-    measurementId: "G-532V0Z5KP3"
+    apiKey: "*********************",
+    authDomain: "*********************",
+    databaseURL: "*******************",
+    projectId: "*******************",
+    storageBucket: "*******************",
+    messagingSenderId: "*******************",
+    appId: "*******************",
+    measurementId: "*******************"
   };
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 //añadir timestamp
 firebase.database.ServerValue.TIMESTAMP
 export {firebase}
+
